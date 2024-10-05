f61e18402c9aaa8ab107febe7d5cffb7807727ad
RANDRIANAINA Georges Aaron <randrianaina.georgesaaron@gmail.com>
2024-10-05 18:05:18 +0200

    set my new colour preference

--- config.def.h
+++ config.def.h
@@ -177,54 +177,36 @@ float alphaUnfocused = 0.6;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	/* "black", */
-	/* "red3", */
-	/* "green3", */
-	/* "yellow3", */
-	/* "blue2", */
-	/* "magenta3", */
-	/* "cyan3", */
-	/* "gray90", */
+  /* 8 normal colors */
+  "black",
+  "darkred",
+  "darkseagreen4",
+  "darkgoldenrod",
+  "steelblue4",
+  "darkmagenta",
+  "cyan4",
+  "white",
+  /* 8 bright colors */
+  "black",
+  "firebrick",
+  "palegreen4",
+  "goldenrod",
+  "steelblue3",
+  "magenta3",
+  "cyan4",
+  "white",
 
-    "#222D31",
-    "#850404",
-    "#3D893D",
-    "#9B9C11",
-    "#44849C",
-    "#925E86",
-    "#0A8B71",
-    "#d8d8d8",
+  [255] = 0,
 
-	/* 8 bright colors */
-	/* "gray50", */
-	/* "red", */
-	/* "green", */
-	/* "yellow", */
-	/* "#5c5cff", */
-	/* "magenta", */
-	/* "cyan", */
-	/* "white", */
-
-    "#585858",
-    "#911D1D",
-    "#5AA55A",
-    "#C29C28", /* "#f1c232", */
-    "#5690A5",
-    "#9C6E92",
-    "#3AA28D",
-    "#f8f8f8",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-    "#1ABB9B", /* ↓ */
-	/* "#add8e6", /\* 256 -> cursor *\/ */
-	"#555555", /* 257 -> rev cursor*/
-    "#d3d3d3", /* ↓ */
-	/* "#222D31", /\* 258 -> bg *\/ */
-    "black", /* ↓ */
-	/* "#e5e5e5", /\* 259 -> fg *\/ */
+  /* more colors can be added after 255 to use with DefaultXX */
+  /* 256 -- cursor */
+  "black",
+  /* 257 -- rev curosr */
+  "#555555",
+  /* 258 -- background */
+  "whitesmoke",
+  /* 259 -- foreground */
+  "black"
 };
 
 
